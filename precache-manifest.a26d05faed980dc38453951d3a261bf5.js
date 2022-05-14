87c3d1c04e1b10a1b8a6139656b7d313570fa026
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 15:35:58 -0600

    Updates

---
+++ precache-manifest.a26d05faed980dc38453951d3a261bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b06bdafe463f42971fbdb994097573",
+    "url": "/React-Birthday-Reminder/index.html"
+  },
+  {
+    "revision": "c3e7aac64e2f2ad60e4d",
+    "url": "/React-Birthday-Reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/React-Birthday-Reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Birthday-Reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e7aac64e2f2ad60e4d",
+    "url": "/React-Birthday-Reminder/static/js/main.510b0c15.chunk.js"
+  },
+  {
+    "revision": "4ac10d08ba3a1a92b503",
+    "url": "/React-Birthday-Reminder/static/js/runtime-main.3597048c.js"
+  }
+]);
