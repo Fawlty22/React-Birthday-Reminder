f24e9f5decbf1ba0d7c0450730c7473060d77492
Matt Keys <MatthewjKeys22@gmail.com>
2022-05-14 15:26:58 -0600

    initial commit with react app and files created

---
+++ setup/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data)
+
+  return <main>
+main
+  </main>;
+}
+
+export default App;
